2e57423741fc5882945a77941e7c3c56eb298303
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-03 22:56:32 +0530

    multiplecation program

---
+++ multiplecation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int multiplecation ;
+    multiplecation = a*b;
+    printf("\n multiplecation = %d", multiplecation);
+    return 0;
+}
