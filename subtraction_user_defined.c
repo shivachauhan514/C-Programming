9fc6ba943b89c7bf9ec5d93bdac83641ff227124
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 15:01:02 +0530

    Add subtraction_user_defined.c

---
+++ subtraction_user_defined.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int sub;
+    printf("\n enter the first number a=");
+    scanf("%d", &a);
+    printf("\n enter the second number b=");
+    scanf("%d", &b);
+    sub = a - b;
+    printf("\n subtraction= %d",sub);
+    return 0;
+}
