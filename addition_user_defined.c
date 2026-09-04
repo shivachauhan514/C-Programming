f9b8a4591b4bfa71e41ac6e6c87deac6921adf86
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 21:51:32 +0530

    Add addition_user-defined program

---
+++ addition_user_defined.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    printf("\n enter the first number a=");
+    scanf("%d", &a);
+    printf("\n enter the second number b=");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("\n addition= %d",sum);
+    return 0;
+}
