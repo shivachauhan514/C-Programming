997e5463d3383372d55b3b63015bea2fa2517b7b
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 21:47:16 +0530

    Add division_user_defined program

---
+++ division_user_defined.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int div;
+    printf("\n enter the first number a=");
+    scanf("%d", &a);
+    printf("\n enter the second number b=");
+    scanf("%d", &b);
+    div = a / b;
+    printf("\n division= %d",div);
+    return 0;
+}
