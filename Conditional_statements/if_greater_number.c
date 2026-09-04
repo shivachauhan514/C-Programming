1bb759a73259725f99cc70b671f374a08f2a2cc3
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 22:25:16 +0530

    Added if statement programs

---
+++ Conditional_statements/if_greater_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("\n enter the first number a=");
+    scanf("%d", &a);
+    printf("\n enter the second number b=");
+    scanf("%d", &b);
+    if (a > b)
+    {
+        printf("\n a is greater than b");
+    }
+    return 0;
+}
