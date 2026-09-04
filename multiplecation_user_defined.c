cface4155504b6967a0f5a5f67d5fd1a4c347022
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 21:39:29 +0530

    Add multiplecation_user_defined program

---
+++ multiplecation_user_defined.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int multi;
+    printf("\n enter the first number a=");
+    scanf("%d", &a);
+    printf("\n enter the second number b=");
+    scanf("%d", &b);
+    multi = a * b;
+    printf("\n multiplication= %d",multi);
+    return 0;
+}
