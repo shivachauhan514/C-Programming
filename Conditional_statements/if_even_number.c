6a6f70a1b4363d7ec34c6a965b6e82119047760a
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 22:24:32 +0530

    Added if stetement programs

---
+++ Conditional_statements/if_even_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("\n enter any number n=");
+    scanf("%d", &n); 
+    if (n%2 ==0)
+    {
+        printf("\n the number is even");
+    }
+    return 0;
+}
