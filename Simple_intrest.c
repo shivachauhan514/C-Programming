d4851ebdfeb2bc157041802816bce3792491f9e5
shiva chauhan <shivachauhanj.n.v@gmail.com>
2026-09-04 10:10:18 +0530

    Add simple_intrest program

---
+++ Simple_intrest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int p = 10;
+    int r = 5;
+    int t = 2;
+    int simple_interest;
+    simple_interest = (p * r * t) / 100;
+    printf("\n  simple_interest = %d", simple_interest);
+    return 0;
+}
